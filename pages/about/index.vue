1b0707aeaa7eb96e4bad559ab9f6e1de8af1a8be
Perry Reid <perry.reid@faulknermediagroup.com>
2019-04-25 18:03:30 -0700

    adding new page dir/, ading styles for individual posts

---
+++ pages/about/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="c-page__about">
+
+        <p>TEXT</p>
+    </div>
+</template>
+
+<style scoped>
+.c-page__about{
+    text-align:center;
+    padding:30px;
+}
+</style>
+
---
+++ pages/posts/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="c-page__posts">
+
+    </div>
+</template>
+
+
+<style scoped>
+.c-page__posts{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
