047e72fde326aced06266ac6f19b7a259d304465
Perry Reid <notownpappa@gmail.com>
2019-08-05 21:39:04 -0700

    Nuxt blog video 42, changing callback to Promise()

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="c-page__posts">
-        <PostList />
+        <PostList :posts="loadedPosts"/>
     </div>
 </template>
 
@@ -9,7 +9,35 @@ import PostList from '@/components/Posts/PostList'
 export default {
     components:{
         PostList
-    }
+    },
+  asyncData(context){
+        return new Promise((resolve, reject) => {
+    setTimeout(() => { 
+      resolve({
+      loadedPosts: [
+        {
+          id:'1', 
+          title: "First Post", 
+          previewText:'Our first!', 
+          thumbnail:'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg',
+        },
+        {
+          id:'2', 
+          title: "Second Post", 
+          previewText:'Our second', 
+          thumbnail:'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg',
+        }     
+        ]
+        });
+    }, 1000);
+        })
+    .then(data => {
+      return data
+    })
+      .catch(e => {
+            context.error(new Error())
+        })
+  },
 }
 </script>
 
